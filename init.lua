0cc1dd1e3a7d9201c6e4c1bf0e42c400f976aa6e
Ákos Kovács <AkoKovacs@diligent.com>
2023-07-30 14:14:28 +0200

    Add treeeess

--- init.lua
+++ init.lua
@@ -19,3 +19,13 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+local api = require "nvim-tree.api"
+vim.keymap.set('n', 'tr', api.tree.focus, {})
+
